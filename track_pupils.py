7738abb63defe4087f2c62fd1f09f1f4ee49e8ba
Nate Casale <casalenm924@gmail.com>
2017-02-09 16:18:36 -0500

    Ported over to Python3

--- track_pupils.py
+++ track_pupils.py
@@ -1,4 +1,4 @@
-import cv2 
+import cv2
 import collections
 import math
 
@@ -15,8 +15,8 @@ def findEyeCenter(gray_eye, thresh_eye):
     minBlack = 300
     pupil = [0, 0]
 
-    for i in xrange(dims[0]):
-        for j in xrange(dims[1]):
+    for i in range(dims[0]):
+        for j in range(dims[1]):
             if thresh_eye[i][j]:
                 if gray_eye[i][j] < minBlack:
                     minBlack = gray_eye[i][j]
@@ -28,63 +28,61 @@ def findEyeCenter(gray_eye, thresh_eye):
 def getPupilAvgFromFace(gray_face, eyes, x, y, w, h):
     """
     getPupilAvgFromFace(gray_face, eyes, x, y, w, h): Takes a grey version of face image,
-    a list of eyes, and the x, y, w, h, coordinates of the face. Returns a list [x, y] for 
+    a list of eyes, and the x, y, w, h, coordinates of the face. Returns a list [x, y] for
     the average between the eyes contained in the list.
     """
     pupil_avg = [0,0]
-    for (ex,ey,ew,eh) in eyes:                
+    for (ex,ey,ew,eh) in eyes:
         gray_eye = gray_face[ey:ey+eh, ex:ex+ew] # get eye
         #eye = face[ey:ey+eh, ex:ex+ew]
-        
+
         # apply gaussian blur to image
         blur = cv2.GaussianBlur(gray_eye, (15,15), 3*gray_eye.shape[0])
-        retval, thresh = cv2.threshold(~blur, 150, 255, cv2.THRESH_BINARY) 
-        contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        retval, thresh = cv2.threshold(~blur, 150, 255, cv2.THRESH_BINARY)
+        _,contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cv2.drawContours(thresh, contours, -1, (255, 255, 255), -1)
-        #circles = cv2.HoughCircles(thresh, cv2.cv.CV_HOUGH_GRADIENT, 1, 1) 
+        #circles = cv2.HoughCircles(thresh, cv2.cv.CV_HOUGH_GRADIENT, 1, 1)
         pupil = findEyeCenter(gray_eye, thresh)
 
         # Uncomment to highlight individual pupils.
-        cv2.circle(img, (pupil[1] + ex + x, pupil[0] + ey + y), 2, (0,255,0), -1)
+        cv2.circle(gray_face, (int(pupil[1] + ex + x), int(pupil[0] + ey + y)), 2, (0,255,0), -1)
 
         pupil_avg[0] += pupil[1] + ex + x;
         pupil_avg[1] += pupil[0] + ey + y;
 
     # Compute pupil average
     pupil_avg = [x / len(eyes) for x in pupil_avg]
-    
-    return pupil_avg
-    
-#
-## main function
-#
 
-if __name__ == '__main__':
+    return pupil_avg
+
+def trackPupils():
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
     left_eye_cascade = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
     right_eye_cascade = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
 
     if face_cascade.empty():
-        print "Did not load face classifier."
+        print("Did not load face classifier.")
 
     if eye_cascade.empty():
-        print "Did not load eye classifier."
+        print("Did not load eye classifier.")
 
     if left_eye_cascade.empty():
-        print "Did not load left eye classifier."
+        print("Did not load left eye classifier.")
 
     if right_eye_cascade.empty():
-        print "Did not load right eye classifier."
-    
+        print("Did not load right eye classifier.")
+
     #cap = cv2.VideoCapture('/Users/bsoper/Movies/eye_tracking/cal_1.mov')
     cap = cv2.VideoCapture(0)
 
     #center_count = 0
     have_center = False
     center = [0,0]
-    rolling_pupil_avg = collections.deque(maxlen=3)
+    rolling_pupil_avg = collections.deque(maxlen=5)
     blink_count = 0
+    x_scale_factor = 60
+    y_scale_factor = 60
 
     while(cap.isOpened()):
 
@@ -93,6 +91,9 @@ if __name__ == '__main__':
         img = cv2.flip(img,1)
 
         #Greyscale
+        if(img.size == 0):
+            continue
+
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # get faces
@@ -104,7 +105,7 @@ if __name__ == '__main__':
             # Pull face sub-image
             gray_face = gray[y:y+h, x:x+w]
             face = img[y:y+h, x:x+w]
-            
+
             eyes = eye_cascade.detectMultiScale(gray_face, 3.0, 5) # locate eye regions
             left_eye = left_eye_cascade.detectMultiScale(gray_face, 3.0, 5) # locate left eye regions
             right_eye = right_eye_cascade.detectMultiScale(gray_face, 3.0, 5) # locate left eye regions
@@ -121,22 +122,22 @@ if __name__ == '__main__':
                     continue
                 else:
                     if blink_count >= 7:
-                        print '\nLong Blink'
+                        print('\nLong Blink')
                         blink_count = 0
                     elif blink_count >= 2:
-                        print '\nBlink'
+                        print('\nBlink')
                         blink_count = 0
                     else:
                         blink_count = 0
-            
+
             if len(eyes) != 2:
                 continue
 
             # Uncomment to add boxes around face and eyes.
-            #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-            #color_face = img[y:y+h, x:x+w]
-            #for (ex,ey,ew,eh) in eyes:
-            #    cv2.rectangle(color_face,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            color_face = img[y:y+h, x:x+w]
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(color_face,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
             pupil_avg = getPupilAvgFromFace(gray_face, eyes, x, y, w, h)
 
@@ -146,20 +147,20 @@ if __name__ == '__main__':
 
             # Highlight center.
             if have_center:
-                cv2.circle(img, (center[0], center[1]), 2, (255,0,0), -1)
-            x_scaled = center[0] + (pupil_avg[0] - center[0]) * 80
-            y_scaled = center[1] + (pupil_avg[1] - center[1]) * 80
+                cv2.circle(img, (int(center[0]), int(center[1])), 2, (255,0,0), -1)
+            x_scaled = center[0] + (pupil_avg[0] - center[0]) * x_scale_factor
+            y_scaled = center[1] + (pupil_avg[1] - center[1]) * y_scale_factor
 
             rolling_pupil_avg.appendleft((x_scaled, y_scaled))
-            
+
             avgs = (sum(a) for a in zip(*rolling_pupil_avg))
             avgs = [a / len(rolling_pupil_avg) for a in avgs]
 
-            cv2.circle(img, (avgs[0], avgs[1]), 5, (0,0,255), -1)
+            cv2.circle(img, (int(avgs[0]), int(avgs[1])), 5, (0,0,255), -1)
             #if avgs[0] - center[0] < -200:
-            #    print 'Left'
+            #    print('Left')
             #elif avgs[0] - center[0] > 200:
-            #    print 'Right'
+            #    print('Right')
 
             # Uncomment to see location without averaging
             #cv2.circle(img, (x_scaled, y_scaled), 5, (255,0,255), -1)
@@ -177,6 +178,12 @@ if __name__ == '__main__':
         if cv2.waitKey(1) & 0xFF  == ord('k'):
             cap.release()
             break
-# cleanup
-cap.release()
-cv2.destroyAllWindows()
+    # cleanup
+    cap.release()
+    cv2.destroyAllWindows()
+#
+## main function
+#
+
+if __name__ == '__main__':
+    trackPupils()
